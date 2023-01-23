d6fe3ee0a284ef9130c5c91eb93a4f230fc40911
AlexeyLozovoy <gvozdik@ua.fm>
2023-01-23 12:17:04 +0200

    ?

--- main.py
+++ main.py
@@ -22,14 +22,13 @@ class Car:
         self.colour = сolour
         self.price = price
 
-car1 = Car("X","2022", "Tesla", "2.0", "black", "23000")
+car1 = Car("X","2022", "Tesla", "Electric", "black", "23000")
 car2 = Car("Corsa","2000", "Opel", "1.2", "blue", "10000")
 
 print(car1.price)
 print(car2.engine_capacity)
 print(car1.engine_capacity)
 print(car2.model)
-print(car1.colour)
 
 # Задание 2
 # Реализуйте класс «Книга». Необходимо хранить в
@@ -43,6 +42,23 @@ print(car1.colour)
 # Реализуйте методы класса для ввода данных, вывода данных,
 # реализуйте доступ к отдельным полям через методы класса.
 
+class Book:
+    def __init__(self, title, year, publisher, genre, author, price):
+        self.title = title
+        self.year = year
+        self.publisher = publisher
+        self.genre = genre
+        self.author = author
+        self.price = price
+
+book1 = Book("Книга1","2022", "Издательство1", "Жанр1", "Автор1", "23")
+book2 = Book("Книга2","2000", "Издательство2", "Жанр2", "Автор2", "10")
+
+print(book1.price)
+print(book2.publisher)
+print(book1.publisher)
+print(book2.author)
+
 # Задание 3
 # Реализуйте класс «Стадион». Необходимо хранить в
 # полях класса: 
@@ -53,3 +69,19 @@ print(car1.colour)
 # вместимость. 
 # Реализуйте методы класса для ввода данных, вывода данных, 
 # реализуйте доступ к отдельным полям через методы класса.
+
+class Stadium:
+    def __init__(self, title, year, country, town, capacity):
+        self.title = title
+        self.year = year
+        self.country = country
+        self.town = town
+        self.capacity = capacity
+        
+stadium1 = Stadium("Уэмбли","2007", "Великобритания", "Лондон", "90 000")
+stadium2 = Stadium("Днепр Арена","2008", "Украина", "Днепр", "34 000")
+
+print(stadium1.title)
+print(stadium2.capacity)
+print(stadium1.capacity)
+print(stadium2.town)
